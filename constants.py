8c564deabd034ca2e1d6f169cd74a4f45f13b90b
Gold Mesa <matfei.ivanov@gmail.com>
2019-08-22 01:02:02 +0300

    Закончен проект, осталось вставить сохранения Bool_format, переделать все в формат exe.

--- constants.py
+++ constants.py
@@ -26,10 +26,110 @@ BASIC_STATS: List[Tuple] = [(0x24, 'HPBase', FLOAT_FORMAT),
                             (0x100, 'EvasionScaling', FLOAT_FORMAT),
                             (0x104, 'Weight', FLOAT_FORMAT),
                             (0x108, 'WeightScaling', FLOAT_FORMAT),
-                            (0x70, 'CritBase', FLOAT_FORMAT),
-                            (0x8C, 'CritScaling', FLOAT_FORMAT),
-                            (0x74, 'KBBase', FLOAT_FORMAT),
-                            (0x90, 'KBScaling', FLOAT_FORMAT),
+                            (0x6C, 'CritBase', FLOAT_FORMAT),
+                            (0x88, 'CritScaling', FLOAT_FORMAT),
+                            (0x70, 'KBBase', FLOAT_FORMAT),
+                            (0x8C, 'KBScaling', FLOAT_FORMAT),
+                            (0x74, 'StaggerRate', FLOAT_FORMAT),
+                            (0x90, 'StaggerRateScaling', FLOAT_FORMAT),
+                            (0x78, 'FireRate', FLOAT_FORMAT),
+                            (0x94, 'FireRateScaling', FLOAT_FORMAT),
+                            (0x7C, 'SleepRate', FLOAT_FORMAT),
+                            (0x98, 'SleepRateScaling', FLOAT_FORMAT),
+                            (0x80, 'FreezeRate', FLOAT_FORMAT),
+                            (0x9C, 'FreezeRateScaling', FLOAT_FORMAT),
+                            (0x84, 'PoisonRate', FLOAT_FORMAT),
+                            (0xA0, 'PoisonRateScaling', FLOAT_FORMAT),
+                            (0x144, 'CritRes', FLOAT_FORMAT),
+                            (0x160, 'CritResScaling', FLOAT_FORMAT),
+                            (0x148, 'KBRes', FLOAT_FORMAT),
+                            (0x164, 'KBResScaling', FLOAT_FORMAT),
+                            (0x14C, 'StaggerRes', FLOAT_FORMAT),
+                            (0x168, 'StaggerResScaling', FLOAT_FORMAT),
+                            (0x150, 'FireRes', FLOAT_FORMAT),
+                            (0x16C, 'FireResScaling', FLOAT_FORMAT),
+                            (0x154, 'SleepRes', FLOAT_FORMAT),
+                            (0x170, 'SleepResScaling', FLOAT_FORMAT),
+                            (0x158, 'FreezeRes', FLOAT_FORMAT),
+                            (0x174, 'FreezeResScaling', FLOAT_FORMAT),
+                            (0x15C, 'PoisonRes', FLOAT_FORMAT),
+                            (0x178, 'PoisonResScaling', FLOAT_FORMAT),
+                            (0xB4, 'DemonsX', FLOAT_FORMAT),
+                            (0xD4, 'DemonsXScaling', FLOAT_FORMAT),
+                            (0xB8, 'UndeadX', FLOAT_FORMAT),
+                            (0xD8, 'UndeadXScaling', FLOAT_FORMAT),
+                            (0xBC, 'ShellsX', FLOAT_FORMAT),
+                            (0xDC, 'ShellsXScaling', FLOAT_FORMAT),
+                            (0xC0, 'DragonsX', FLOAT_FORMAT),
+                            (0xE0, 'DragonsXScaling', FLOAT_FORMAT),
+                            (0xC4, 'GiantsX', FLOAT_FORMAT),
+                            (0xE4, 'GiantsXScaling', FLOAT_FORMAT),
+                            (0xC8, 'WoodX', FLOAT_FORMAT),
+                            (0xE8, 'WoodXScaling', FLOAT_FORMAT),
+                            (0xCC, 'StoneX', FLOAT_FORMAT),
+                            (0xEC, 'StoneXScaling', FLOAT_FORMAT),
+                            (0xD0, 'MetalsX', FLOAT_FORMAT),
+                            (0xF0, 'MetalsXScaling', FLOAT_FORMAT),
+                            (0x118, 'SlashX', FLOAT_FORMAT),
+                            (0x11C, 'StrikeX', FLOAT_FORMAT),
+                            (0x120, 'StabX', FLOAT_FORMAT),
+                            (0x124, 'CrushX', FLOAT_FORMAT),
+                            (0x128, 'FlameX', FLOAT_FORMAT),
+                            (0x12C, 'IceX', FLOAT_FORMAT),
+                            (0x130, 'ShockX', FLOAT_FORMAT),
+                            (0x134, 'PoisonX', FLOAT_FORMAT),
+                            (0x138, 'SoundX', FLOAT_FORMAT),
+                            (0x13C, 'LightX', FLOAT_FORMAT),
+                            (0x13C, 'DarkX', FLOAT_FORMAT),
+
+
                             ]
-CHECK_BOXES: List[Tuple] = [(0xA5, 'Crush', BOOL_FORMAT),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CHECK_BOXES: List[Tuple] = [(0xA4, 'Slash', BOOL_FORMAT),
+                            (0xA5, 'Strike', BOOL_FORMAT),
+                            (0xA6, 'Stab', BOOL_FORMAT),
+                            (0xA7, 'Crush', BOOL_FORMAT),
+                            (0xA8, 'Fire', BOOL_FORMAT),
+                            (0xA9, 'Ice', BOOL_FORMAT),
+                            (0xAA, 'Lightning', BOOL_FORMAT),
+                            (0xAB, 'Poison', BOOL_FORMAT),
+                            (0xAC, 'Sound', BOOL_FORMAT),
+                            (0xAD, 'Light', BOOL_FORMAT),
+                            (0xAE, 'Darkness', BOOL_FORMAT),
+                            (0xAF, 'Burn', BOOL_FORMAT),
+                            (0xB0, 'Venom', BOOL_FORMAT),
+                            (0x17E, 'CritImm', BOOL_FORMAT),
+                            (0x17F, 'StaggerImm', BOOL_FORMAT),
+                            (0x180, 'KBImm', BOOL_FORMAT),
+                            (0x181, 'FireImm', BOOL_FORMAT),
+                            (0x182, 'SleepImm', BOOL_FORMAT),
+                            (0x183, 'IceImm', BOOL_FORMAT),
+                            (0x184, 'PoisonImm', BOOL_FORMAT),
+                            (0x185, 'TumbleImm', BOOL_FORMAT),
+
+
+
+
+
                             ]
+
+
+
+
--- convert_ui.cmd
+++ convert_ui.cmd
@@ -1,4 +1,3 @@
 rem C:\Users\matfe\AppData\Local\Programs\Python\Python37\Scripts\designer.exe
 C:\Users\matfe\AppData\Local\Programs\Python\Python37\Scripts\pyuic5 editor.ui -o editor.py
 rem C:\Users\matfe\AppData\Local\Programs\Python\Python37\Scripts\pyinstaller -y  -w -F  "C:/Users/matfe/PycharmProjects/PSP_patapon3/patapon_editor.py"
-
